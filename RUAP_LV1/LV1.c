65325d91e4d2f4b43b5e3075c607d5630b680ea4
Tomislav Cajbert <tcajbert@gmail.com>
2018-11-05 23:42:11 +0100

    vracanje na prvi ispis

--- RUAP_LV1/LV1.c
+++ RUAP_LV1/LV1.c
@@ -1,5 +1,9 @@
 #include "stdio.h"
 
 int main(){
+<<<<<<< HEAD
 printf("Pozdrav iz aplikacije od Tomislav Cajbert, broj ideksa je AI4368");
+=======
+printf("Pozdrav iz aplikacije od Tomislav Cajbert");
+>>>>>>> 3bd35a3... ispisivanje pozdrava iz aplikacije
 }
